105ef6d9693076b49f266632771bba3babb9afea
Ryan Roman <rtroman14@gmail.com>
2020-02-18 09:30:12 -0600

    nodemailer working

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 const app = express();
 
@@ -25,72 +26,41 @@ app.get("/about", (req, res) => {
     res.render("about");
 });
 
-// app.post("/send", (req, res) => {
-//     const output = `
-//     <p>You have a new contact request</p>
-//     <h3>Contact Details</h3>
-//     <ul>
-//         <li>Name: ${req.body.name} </li>
-//         <li>Email: ${req.body.email} </li>
-//     </ul>
-//     <p>Message: ${req.body.message}</p>
-//     `;
+app.post("/send", (req, res) => {
+    const output = `
+    <p>You have a new contact request!</p>
+    <h3>Contact Details</h3>
+    <ul>
+        <li>Name: ${req.body.name} </li>
+        <li>Email: ${req.body.email} </li>
+    </ul>
+    <p>Message: ${req.body.message}</p>
+    `;
 
-//     // let transporter = nodemailer.createTransport({
-//     //     host: "",
-//     //     port: 587,
-//     //     secure: false,
-//     //     auth: {
-//     //         user: "rroman@cr-bps.com",
-//     //         pass: "123546Timothy!"
-//     //     }
-//     // });
+    let transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.GMAIL_USERNAME_SENDER,
+            pass: process.env.GMAIL_PASSWORD_SENDER
+        }
+    });
 
-//     // let mailOptions = {
-//     //     from: '"Nodemailer Contact" <rroman@cr-bps.com>',
-//     //     to: "rtroman14@gmail.com",
-//     //     subject: "Portfolio Contact Request",
-//     //     text: "Hello World",
-//     //     html: output
-//     // };
+    let mailOptions = {
+        from: process.env.GMAIL_USERNAME_SENDER,
+        to: process.env.GMAIL_USERNAME_SENDEE,
+        subject: "Portfolio Website Contact Request",
+        html: output
+    };
 
-//     // // send mail with defined transport object
-//     // transporter.sendMail(mailOptions, (error, info) => {
-//     //     if (error) {
-//     //         return console.log(error);
-//     //     }
-//     //     console.log("Message sent: %s", info.messageId);
-//     //     console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("Email sent: " + info.response);
+        }
+    });
 
-//     //     // res.render("contact", { msg: "Email has been sent!" });
-//     // });
-// });
-
-// app.post("/send", (req, res) => {
-//     console.log(req.body);
-// });
-
-var transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: "rtroman14@gmail.com",
-        pass: "123546Timothy"
-    }
-});
-
-var mailOptions = {
-    from: "rtroman14@gmail.com",
-    to: "rroman@cr-bps.com",
-    subject: "Sending Email using Node.js",
-    text: "That was easy!"
-};
-
-transporter.sendMail(mailOptions, function(error, info) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log("Email sent: " + info.response);
-    }
+    res.redirect("/");
 });
 
 let port = process.env.PORT || 3000;
